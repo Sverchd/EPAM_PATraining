ffa5d53fb82aac295e2d7521db21cf9c4900e721
Dmytro Sverchkov <dmytro_sverchkov@epam.com>
2021-02-09 16:45:21 +0200

    Rollback transaction task upload

---
+++ RollbackTransaction/RollbackTransaction.Plugin/RollbackTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace RollbackTransaction.Plugin
+{
+    public class RollbackTransaction : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext) serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var serviceFactory =
+                (IOrganizationServiceFactory) serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+            var tracingService =
+                (ITracingService) serviceProvider.GetService(typeof(ITracingService));
+
+            var target = (Entity) context.InputParameters["Target"];
+            var report = new Entity
+            {
+                LogicalName = "cr90f_report",
+                Id = Guid.NewGuid(),
+                ["cr90f_description"] = target["cr90f_description"],
+                ["cr90f_priority"] = target["cr90f_priority"],
+                ["cr90f_name"] = target["cr90f_name"]
+            };
+            service.Create(report);
+            tracingService.Trace("In Stage 20 New report created");
+        }
+    }
+}
---
+++ RollbackTransaction/RollbackTransaction.Plugin/ThrowException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace RollbackTransaction.Plugin
+{
+    public class ThrowException : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext) serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var tracingService =
+                (ITracingService) serviceProvider.GetService(typeof(ITracingService));
+            tracingService.Trace("In Stage 40");
+            var target = (Entity) context.InputParameters["Target"];
+
+            if ((int) target["cr90f_priority"] == 2)
+            {
+                tracingService.Trace("Throwing exception");
+                throw new InvalidPluginExecutionException("Revert");
+            }
+        }
+    }
+}
