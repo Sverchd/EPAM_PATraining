2a9e3603a90d1cf40f09818313e27ae7871466ba
Dmytro Sverchkov <dmytro_sverchkov@epam.com>
2021-02-09 17:28:36 +0200

    Pre Image usage

---
+++ PreImageUsage/PreImageUsage.Plugin/PreImageUsagePlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace PreImageUsage.Plugin
+{
+    public class PreImageUsagePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext) serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context.MessageName.Equals("Update"))
+            {
+                var tracingService =
+                    (ITracingService) serviceProvider.GetService(typeof(ITracingService));
+
+                if (context.Stage == 40)
+                {
+                    tracingService.Trace("In Stage 40");
+                    var target = (Entity) context.InputParameters["Target"];
+                    var preMessageImage = context.PreEntityImages["NewPreImage"];
+
+                    tracingService.Trace("Old value: " + preMessageImage["cr90f_description"]);
+                    tracingService.Trace("New value: " + target["cr90f_description"]);
+                }
+            }
+        }
+    }
+}
